7df03929d50f06b10e6d06ce66babeb6c4106538
robox <root@roboxv.pro>
2017-12-08 12:05:25 +0600

    Add day 8 solution

--- aoc2017/d8/main2.py
+++ aoc2017/d8/main2.py
@@ -6,6 +6,7 @@ class Day(AoCFramework):
     test_cases = (
     )
     registers = {}
+    historical_max = 0
 
     def do_operation(self, register, operation, amount, c_register, c_sign, c_amount):
         amount, c_amount = int(amount), int(c_amount)
@@ -21,6 +22,7 @@ class Day(AoCFramework):
         }
         if comparison_signs[c_sign](self.registers[c_register], c_amount):
             self.registers[register] += amount if operation == 'inc' else -amount
+            self.historical_max = max(self.registers[register], self.historical_max)
         pass
 
     def go(self):
@@ -28,8 +30,7 @@ class Day(AoCFramework):
         for command in self.linesplitted:
             match = re.match(pattern, command)
             self.do_operation(**match.groupdict())
-
-        answer = max(self.registers.values())
+        answer = self.historical_max
         return answer
 
 
